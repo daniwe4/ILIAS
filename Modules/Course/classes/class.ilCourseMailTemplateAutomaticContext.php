962e7e4508fcee66d9199a8b1336f281e35a20dd
Richard Klees <richard.klees@concepts-and-training.de>
2017-10-23 14:56:40 +0200

    TMS: implemented basic facilities for mailing
    * database-interface for templates
    * placeholders via ente
    * logging for mails
    * interfaces for building mail-content

--- Modules/Course/classes/class.ilCourseMailTemplateAutomaticContext.php
+++ Modules/Course/classes/class.ilCourseMailTemplateAutomaticContext.php
@@ -3,30 +3,36 @@
 
 /* Copyright (c) 1998-2009 ILIAS open source, Extended GPL, see docs/LICENSE */
 
+use CaT\Ente\ILIAS\ilHandlerObjectHelper;
+
 include_once './Services/Mail/classes/class.ilMailTemplateContext.php';
 
 /**
  * Handles course mail placeholders
  *
  * @author Nils Haagen <nils.haagen@concepts-and-training.de>
+ * @author Stefan Hecken <stefan.hecken@concepts-and-training.de>
  * @package ModulesCourse
  */
 class ilCourseMailTemplateAutomaticContext extends ilMailTemplateContext
 {
+    use ilHandlerObjectHelper;
+
     const ID = 'crs_context_automatic';
+    const REPOSITORY_REF_ID = 1;
 
     /**
-     * @return string
+     * @inheritdoc
      */
-    public function getId()
+    public function getId() : string
     {
         return self::ID;
     }
 
     /**
-     * @return string
+     * @inheritdoc
      */
-    public function getTitle()
+    public function getTitle() : string
     {
         global $lng;
 
@@ -36,9 +42,9 @@ class ilCourseMailTemplateAutomaticContext extends ilMailTemplateContext
     }
 
     /**
-     * @return string
+     * @inheritdoc
      */
-    public function getDescription()
+    public function getDescription() : string
     {
         global $lng;
 
@@ -48,10 +54,9 @@ class ilCourseMailTemplateAutomaticContext extends ilMailTemplateContext
     }
 
     /**
-     * Return an array of placeholders
-     * @return array
+     * @inheritdoc
      */
-    public function getSpecificPlaceholders()
+    public function getSpecificPlaceholders() : array
     {
         /**
          * @var $lng ilLanguage
@@ -72,13 +77,40 @@ class ilCourseMailTemplateAutomaticContext extends ilMailTemplateContext
             'label' => $lng->txt('crs_mail_permanent_link')
         );
 
+        foreach ($this->getComponentPlaceholders() as $comp_placeholder) {
+            $placeholders[$comp_placeholder->getPlaceholder()] = array(
+                    'placeholder' => $comp_placeholder->getPlaceholder(),
+                    'label' => $comp_placeholder->getDescription()
+                );
+        }
+
+
+        foreach ($this->getGloballyProvidedMailContexts() as $context) {
+            foreach ($context->placeholderIds() as $placeholder_id) {
+                $id = get_class($context) . $placeholder_id;
+                $placeholders[$id] = array(
+                    'placeholder' => $placeholder_id,
+                    'label' => get_class($context)
+                );
+            }
+        }
+
+        foreach ($this->getTMSStandardPlaceholderIds() as $context => $ids) {
+            foreach ($ids as $id) {
+                $placeholders[$context . $id] = array(
+                        'placeholder' => $id,
+                        'label' => $context
+                    );
+            }
+        }
+
         return $placeholders;
     }
 
     /**
-     * {@inheritdoc}
+     * @inheritdoc
      */
-    public function resolveSpecificPlaceholder($placeholder_id, array $context_parameters, ilObjUser $recipient = null, $html_markup = false)
+    public function resolveSpecificPlaceholder($placeholder_id, array $context_parameters, ilObjUser $recipient = null, $html_markup = false) : string
     {
         /**
          * @var $ilObjDataCache ilObjectDataCache
@@ -92,7 +124,82 @@ class ilCourseMailTemplateAutomaticContext extends ilMailTemplateContext
             return ilLink::_getLink($context_parameters['ref_id'], 'crs');
         }
 
+        $placeholder_values = $this->getComponentPlaceholdersValues();
+        $placeholder = array_filter(
+            $placeholder_values,
+            function ($p) use ($placeholder_id) {
+                if ($p->getPlaceholder() == $placeholder_id) {
+                    return $p;
+                }
+            }
+        );
+
+        if (count($placeholder) == 1) {
+            $p = array_shift($placeholder);
+            return $p->getValue();
+        }
+
         return '';
+    }
+
+    /**
+     * Get all mail placeholder via CaT\Ente
+     *
+     * @return \ILIAS\TMS\Mailing\MailPlaceholder[]
+     */
+    protected function getComponentPlaceholders()
+    {
+        return $this->getComponentsOfType(\ILIAS\TMS\Mailing\Placeholder::class);
+    }
+
+    /**
+     * Get all mail placeholder values via CaT\Ente
+     *
+     * @return MailPlaceholderValue[]
+     */
+    protected function getComponentPlaceholdersValues()
+    {
+        return $this->getComponentsOfType(\ILIAS\TMS\Mailing\PlaceholderValue::class);
+    }
+
+
+    /**
+     * Get all mailing contexts from Ente
+     *
+     * @return MailContext[]
+     */
+    protected function getGloballyProvidedMailContexts()
+    {
+        return $this->getComponentsOfType(ILIAS\TMS\Mailing\MailContext::class);
+    }
+
+    /**
+     * Get placeholderids of TMS-Standard contexts
+     *
+     * @return array<string, string[]>
+     */
+    protected function getTMSStandardPlaceholderIds()
+    {
+        require_once('./Services/TMS/Mailing/classes/ilTMSMailing.php');
+        $tms_mailing = new \ilTMSMailing();
+        return $tms_mailing->getPlaceholderIdsOfStandardContexts();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function getEntityRefId()
+    {
+        return self::REPOSITORY_REF_ID;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function getDIC()
+    {
+        global $DIC;
+        return $DIC;
     }
 }
 
--- Services/TMS/Booking/classes/class.ilTMSBookingActions.php
+++ Services/TMS/Booking/classes/class.ilTMSBookingActions.php
@@ -4,157 +4,171 @@
  */
 
 use ILIAS\TMS\Booking;
+use ILIAS\TMS\Mailing;
 
 /* Copyright (c) 2017 Richard Klees <richard.klees@concepts-and-training.de> */
 
 /**
  * This is how the booking of users really works. Dark magic happening here.
  */
-class ilTMSBookingActions implements Booking\Actions {
-	/**
-	 * @inheritdoc
-	 */
-	public function bookUser($crs_ref_id, $user_id) {
-		$course = ilObjectFactory::getInstanceByRefId($crs_ref_id);
-		assert('$course instanceof \ilObjCourse');
-		$user = ilObjectFactory::getInstanceByObjId($user_id);
-		assert('$user instanceof \ilObjUser');
+class ilTMSBookingActions implements Booking\Actions
+{
+    public function __construct(Mailing\Actions $mailing)
+    {
+        $this->mailing = $mailing;
+    }
+    /**
+     * @inheritdoc
+     */
+    public function bookUser($crs_ref_id, $user_id)
+    {
+        $course = ilObjectFactory::getInstanceByRefId($crs_ref_id);
+        assert('$course instanceof \ilObjCourse');
+        $user = ilObjectFactory::getInstanceByObjId($user_id);
+        assert('$user instanceof \ilObjUser');
 
-		return $this->maybeMakeCourseMember($course, $user);
-	}
+        return $this->maybeMakeCourseMember($course, $user);
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	public function cancelUser($crs_ref_id, $user_id) {
-		require_once("Modules/Course/classes/class.ilCourseParticipants.php");
-		require_once("Services/Membership/classes/class.ilWaitingList.php");
-		$course = ilObjectFactory::getInstanceByRefId($crs_ref_id);
-		if(ilCourseParticipants::_isParticipant($course->getRefId(), $user_id)) {
-			$course->getMemberObject()->delete($user_id);
-			$this->mailing->sendCourseMail(Mailing\Actions::CANCELED_FROM_COURSE, $course->getRefId(), $user_id);
-			return Booking\Actions::STATE_REMOVED_FROM_COURSE;
-		}
+    /**
+     * @inheritdoc
+     */
+    public function cancelUser($crs_ref_id, $user_id)
+    {
+        require_once("Modules/Course/classes/class.ilCourseParticipants.php");
+        require_once("Services/Membership/classes/class.ilWaitingList.php");
+        $course = ilObjectFactory::getInstanceByRefId($crs_ref_id);
+        if (ilCourseParticipants::_isParticipant($course->getRefId(), $user_id)) {
+            $course->getMemberObject()->delete($user_id);
+            $this->mailing->sendCourseMail(Mailing\Actions::CANCELED_FROM_COURSE, $course->getRefId(), $user_id);
+            return Booking\Actions::STATE_REMOVED_FROM_COURSE;
+        }
 
-		$crs_id = $course->getId();
-		if(ilWaitingList::_isOnList($user_id, $crs_id)) {
-			ilWaitingList::deleteUserEntry($user_id, $crs_id);
-			$this->mailing->sendCourseMail(Mailing\Actions::CANCELED_FROM_WAITINGLIST, $course->getRefId(), $user_id);
-			return Booking\Actions::STATE_REMOVED_FROM_WAITINGLIST;
-		}
+        $crs_id = $course->getId();
+        if (ilWaitingList::_isOnList($user_id, $crs_id)) {
+            ilWaitingList::deleteUserEntry($user_id, $crs_id);
+            $this->mailing->sendCourseMail(Mailing\Actions::CANCELED_FROM_WAITINGLIST, $course->getRefId(), $user_id);
+            return Booking\Actions::STATE_REMOVED_FROM_WAITINGLIST;
+        }
 
-		throw new \LogicException("User can not be canceld. Not booked on course or waitinglist.");
-	}
+        throw new \LogicException("User can not be canceld. Not booked on course or waitinglist.");
+    }
 
-	/**
-	 * Make the user be a member of the course, if he does not already have a member role.
-	 *
-	 * @throws	\LogicException if user does have another role on the course than member
-	 * @throws	\LogicException if user could not booked or added to waitinglist
-	 * @param	\ilObjCourse    $course
-	 * @param	\ilObjUser      $user
-	 * @return	string
-	 */
-	protected function maybeMakeCourseMember(\ilObjCourse $course, \ilObjUser $user) {
-		require_once("Modules/Course/classes/class.ilCourseParticipant.php");
-		$participant = \ilCourseParticipant::_getInstancebyObjId($course->getId(), $user->getId());
-		if ($participant->isMember()) {
-			return Booking\Actions::STATE_BOOKED;
-		}
-		if ($participant->isParticipant()) {
-			throw new \LogicException("User already has a local role on the course. Won't be able to make him a member.");
-		}
+    /**
+     * Make the user be a member of the course, if he does not already have a member role.
+     *
+     * @throws	\LogicException if user does have another role on the course than member
+     * @throws	\LogicException if user could not booked or added to waitinglist
+     * @param	\ilObjCourse    $course
+     * @param	\ilObjUser      $user
+     * @return	string
+     */
+    protected function maybeMakeCourseMember(\ilObjCourse $course, \ilObjUser $user)
+    {
+        require_once("Modules/Course/classes/class.ilCourseParticipant.php");
+        $participant = \ilCourseParticipant::_getInstancebyObjId($course->getId(), $user->getId());
+        if ($participant->isMember()) {
+            return Booking\Actions::STATE_BOOKED;
+        }
+        if ($participant->isParticipant()) {
+            throw new \LogicException("User already has a local role on the course. Won't be able to make him a member.");
+        }
 
-		$booking_modality = $this->getFirstBookingModalities((int)$course->getRefId());
+        $booking_modality = $this->getFirstBookingModalities((int) $course->getRefId());
 
-		if($booking_modality) {
-			require_once("Customizing/global/plugins/Services/Repository/RepositoryObject/BookingModalities/classes/class.ilObjBookingModalities.php");
+        if ($booking_modality) {
+            require_once("Customizing/global/plugins/Services/Repository/RepositoryObject/BookingModalities/classes/class.ilObjBookingModalities.php");
 
-			if($this->maybeBookAsMember((int)$course->getRefId(), $booking_modality)) {
-				$participant->add($user->getId(), IL_CRS_MEMBER);
-				return Booking\Actions::STATE_BOOKED;
-			}
+            if ($this->maybeBookAsMember((int) $course->getRefId(), $booking_modality)) {
+                $participant->add($user->getId(), IL_CRS_MEMBER);
+                $this->mailing->sendCourseMail(Mailing\Actions::BOOKED_ON_COURSE, $course->getRefId(), $user->getId());
+                return Booking\Actions::STATE_BOOKED;
+            }
 
-			if($this->maybeAddOnWaitingList($course, $booking_modality)) {
-				$course->waiting_list_obj->addToList((int)$user->getId());
-				return Booking\Actions::STATE_WAITING_LIST;
-			}
-		}
+            if ($this->maybeAddOnWaitingList($course, $booking_modality)) {
+                $course->waiting_list_obj->addToList((int) $user->getId());
+                $this->mailing->sendCourseMail(Mailing\Actions::BOOKED_ON_WAITINGLIST, $course->getRefId(), $user->getId());
+                return Booking\Actions::STATE_WAITING_LIST;
+            }
+        }
 
-		throw new \LogicException("User can not be booked. Course and waitinglist are overbooked");
-	}
+        throw new \LogicException("User can not be booked. Course and waitinglist are overbooked");
+    }
 
-	/**
-	 * Check user can be booked as member
-	 *
-	 * @param int 	$crs_ref_id
-	 * @param $booking_modality
-	 *
-	 * @return bool
-	 */
-	protected function maybeBookAsMember($crs_ref_id, \ilObjBookingModalities $booking_modality) {
-		require_once("Modules/Course/classes/class.ilCourseParticipants.php");
-		$max_member = $booking_modality->getMember()->getMax();
-		$current_member = \ilCourseParticipants::lookupNumberOfMembers($crs_ref_id);
+    /**
+     * Check user can be booked as member
+     *
+     * @param int 	$crs_ref_id
+     * @param $booking_modality
+     *
+     * @return bool
+     */
+    protected function maybeBookAsMember($crs_ref_id, \ilObjBookingModalities $booking_modality)
+    {
+        require_once("Modules/Course/classes/class.ilCourseParticipants.php");
+        $max_member = $booking_modality->getMember()->getMax();
+        $current_member = \ilCourseParticipants::lookupNumberOfMembers($crs_ref_id);
 
-		if($max_member === null || $current_member < $max_member){
-			return true;
-		}
+        if ($max_member === null || $current_member < $max_member) {
+            return true;
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	/**
-	 * Checks user can be added to waitinglist
-	 *
-	 * @param \ilObjCourse 	$course
-	 * @param $booking_modality
-	 *
-	 * @return bool
-	 */
-	protected function maybeAddOnWaitingList(\ilObjCourse $course, \ilObjBookingModalities $booking_modality) {
-		if ($booking_modality->getWaitinglist()->getModus() == "no_waitinglist") {
-			return false;
-		}
-		$course->initWaitingList();
-		$max_waiting = $booking_modality->getWaitinglist()->getMax();
-		$current_waiting = $course->waiting_list_obj->getCountUsers();
+    /**
+     * Checks user can be added to waitinglist
+     *
+     * @param \ilObjCourse 	$course
+     * @param $booking_modality
+     *
+     * @return bool
+     */
+    protected function maybeAddOnWaitingList(\ilObjCourse $course, \ilObjBookingModalities $booking_modality)
+    {
+        if ($booking_modality->getWaitinglist()->getModus() == "no_waitinglist") {
+            return false;
+        }
+        $course->initWaitingList();
+        $max_waiting = $booking_modality->getWaitinglist()->getMax();
+        $current_waiting = $course->waiting_list_obj->getCountUsers();
 
-		if($max_waiting === null || $current_waiting < $max_waiting) {
-			return true;
-		}
+        if ($max_waiting === null || $current_waiting < $max_waiting) {
+            return true;
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	/**
-	 * Get the first booking modalities below crs
-	 *
-	 * @param int 	$crs_ref_id
-	 *
-	 * @return BookingModalities | null
-	 */
-	protected function getFirstBookingModalities($crs_ref_id) {
-		global $DIC;
-		$tree = $DIC->repositoryTree();
-		$objDefinition = $DIC["objDefinition"];
+    /**
+     * Get the first booking modalities below crs
+     *
+     * @param int 	$crs_ref_id
+     *
+     * @return BookingModalities | null
+     */
+    protected function getFirstBookingModalities($crs_ref_id)
+    {
+        global $DIC;
+        $tree = $DIC->repositoryTree();
+        $objDefinition = $DIC["objDefinition"];
 
-		$childs = $tree->getChilds($crs_ref_id);
-		$ret = array();
-		foreach ($childs as $child) {
-			$type = $child["type"];
-			if($type == "xbkm") {
-				return \ilObjectFactory::getInstanceByRefId($child["child"]);
-				continue;
-			}
+        $childs = $tree->getChilds($crs_ref_id);
+        $ret = array();
+        foreach ($childs as $child) {
+            $type = $child["type"];
+            if ($type == "xbkm") {
+                return \ilObjectFactory::getInstanceByRefId($child["child"]);
+                continue;
+            }
 
-			if($objDefinition->isContainer($type)) {
-				return $this->getFirstBookingModalities($child["child"]);
-			}
-		}
+            if ($objDefinition->isContainer($type)) {
+                return $this->getFirstBookingModalities($child["child"]);
+            }
+        }
 
-		return null;
-	}
+        return null;
+    }
 }
 
 /**
