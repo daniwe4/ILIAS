57d8b0eebe1648ecf46a979ccde361349795a4d3
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2019-04-23 08:17:19 +0200

    TMS: redirect to login even if public area is activated (patch)

--- Services/Init/classes/class.ilInitialisation.php
+++ Services/Init/classes/class.ilInitialisation.php
@@ -819,7 +819,9 @@ class ilInitialisation
      */
     public static function goToPublicSection()
     {
-        global $ilAuth;
+        // cat-tms-patch start #2979
+        global $ilAuth, $ilIliasIniFile;
+        //
 
         if (ANONYMOUS_USER_ID == "") {
             self::abortAndDie("Public Section enabled, but no Anonymous user found.");
@@ -861,10 +863,11 @@ class ilInitialisation
         // we do not know if ref_id of request is accesible, so redirecting to root
         $_GET["ref_id"] = ROOT_FOLDER_ID;
         $_GET["cmd"] = "frameset";
-        self::redirect(
-            "ilias.php?baseClass=ilrepositorygui&reloadpublic=1&cmd=" .
-            $_GET["cmd"] . "&ref_id=" . $_GET["ref_id"]
-        );
+        // cat-tms-patch start #2979
+        $default_client = $ilIliasIniFile->readVariable("clients", "default");
+        $link = "login.php?target=&client_id=" . $default_client . "&cmd=force_login&lang=de";
+        self::redirect($link);
+        // cat-tms-patch end
     }
 
     /**
