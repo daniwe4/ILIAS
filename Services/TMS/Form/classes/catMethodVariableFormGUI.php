68be33dfb8c36a05f54670073c17c0eafac13583
Richard Klees <richard.klees@concepts-and-training.de>
2019-08-19 15:47:33 +0200

    TMS: added catMethodVariableFormGUI to getHTML

---
+++ Services/TMS/Form/classes/catMethodVariableFormGUI.php
@@ -0,0 +1,94 @@
+<?php
+
+class catMethodVariableFormGUI extends ilPropertyFormGUI {
+	/**
+	 * @var string
+	 */
+	protected $method;
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->method = "post";
+	}
+
+	public function setMethod(string $method)
+	{
+		$this->method = $method;
+	}
+
+	public function getMethod(): string
+	{
+		return $this->method;
+	}
+
+	/**
+	* @inheritdoc
+	*/
+	function getHTML()
+	{
+		$tpl = new ilTemplate("tpl.form.html", true, true, "Services/Form");
+		
+		// this line also sets multipart, so it must be before the multipart check
+		$content = $this->getContent();
+		if ($this->getOpenTag())
+		{
+			$opentpl = new ilTemplate('tpl.form_open.html', true, true, "Services/TMS/Form");
+			$opentpl->setVariable("METHOD", $this->getMethod());
+			if ($this->getTarget() != "")
+			{
+				$opentpl->setCurrentBlock("form_target");
+				$opentpl->setVariable("FORM_TARGET", $this->getTarget());
+				$opentpl->parseCurrentBlock();
+			}
+			if ($this->getName() != "")
+			{
+				$opentpl->setCurrentBlock("form_name");
+				$opentpl->setVariable("FORM_NAME", $this->getName());
+				$opentpl->parseCurrentBlock();
+			}
+			if ($this->getPreventDoubleSubmission())
+			{
+				$opentpl->setVariable("FORM_CLASS", "preventDoubleSubmission");
+			}
+
+			if ($this->getMultipart())
+			{
+				$opentpl->touchBlock("multipart");
+
+			}
+			$opentpl->setVariable("FORM_ACTION", $this->getFormAction());
+			if ($this->getId() != "")
+			{
+				$opentpl->setVariable("FORM_ID", $this->getId());
+			}
+			$opentpl->parseCurrentBlock();
+			$tpl->setVariable('FORM_OPEN_TAG', $opentpl->get());
+		}
+		$tpl->setVariable("FORM_CONTENT", $content);
+		if (!$this->getKeepOpen())
+		{
+			$tpl->setVariable("FORM_CLOSE_TAG", "</form>");
+		}
+
+		$html = $tpl->get();
+		
+		// #13531 - get content that has to reside outside of the parent form tag, e.g. panels/layers
+		if(is_array($this->items)) {
+			foreach($this->items as $item)
+			{
+				// #13536 - ilFormSectionHeaderGUI does NOT extend ilFormPropertyGUI ?!
+				if(method_exists($item, "getContentOutsideFormTag"))
+				{
+					$outside = $item->getContentOutsideFormTag();
+					if($outside)
+					{
+						$html .= $outside;
+					}
+				}
+			}
+		}
+
+		return $html;
+	}
+}
