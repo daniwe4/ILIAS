6b954612b6895d2254beac1bcf9727ba163ef6ac
Daniel Weise <daniel.weise@concepts-and-training.de>
2019-11-15 09:29:33 +0100

    tms_4103 - change redirect url and exception handling on cat docukemnt printing system

--- print.php
+++ print.php
@@ -11,24 +11,25 @@ ilInitialisation::initILIAS();
 global $DIC;
 $ctrl = $DIC["ilCtrl"];
 $lng = $DIC["lng"];
+$lng->loadLanguageModule("tms");
 $get = $_GET;
 
 if (!ilPluginAdmin::isPluginActive('docdeliver')) {
-    ilUtil::sendInfo($lng->txt('no_list_available'), true);
-    $ctrl->redirectToUrl("http://localhost/cat_ilias/tms54/goto.php?target=root_1&client_id=tms54");
+    ilUtil::sendInfo($lng->txt('no_document_available'), true);
+    $ctrl->redirectToUrl(ILIAS_HTTP_PATH . "/goto.php?target=root_1&client_id=" . CLIENT_ID);
 }
 
 if (!ilPluginAdmin::isPluginActive('xcmb')) {
-    ilUtil::sendInfo($lng->txt('no_list_available'), true);
-    $ctrl->redirectToUrl("http://localhost/cat_ilias/tms54/goto.php?target=root_1&client_id=tms54");
+    ilUtil::sendInfo($lng->txt('no_document_available'), true);
+    $ctrl->redirectToUrl(ILIAS_HTTP_PATH . "/goto.php?target=root_1&client_id=" . CLIENT_ID);
 }
 
 if (
     !array_key_exists('file', $get) &&
     (is_null($get['file']) || trim($get['file']) == '')
 ) {
-    ilUtil::sendInfo($lng->txt('no_list_available'), true);
-    $ctrl->redirectToUrl("http://localhost/cat_ilias/tms54/goto.php?target=root_1&client_id=tms54");
+    ilUtil::sendInfo($lng->txt('no_document_available'), true);
+    $ctrl->redirectToUrl(ILIAS_HTTP_PATH . "/goto.php?target=root_1&client_id=" . CLIENT_ID);
 }
 
 /** @var ilDocumentDeliveryPlugin $xcmb */
@@ -38,7 +39,15 @@ $file = trim($get['file']);
 $document = null;
 try {
     $document = $doc_deliver->printDocumentForHash($file);
-} catch (\LogicException $e) {
-    ilUtil::sendInfo($lng->txt('no_list_available'), true);
-    $ctrl->redirectToUrl("http://localhost/cat_ilias/tms54/goto.php?target=root_1&client_id=tms54");
+} catch (\Exception $e) {
+    $tpl = $DIC["tpl"];
+    $lng = $DIC["lng"];
+    $tpl->addBlockFile("CONTENT", "content", "tpl.error.html");
+    $lng->loadLanguageModule("error");
+    $tpl->setVariable("HEADER_ICON", ilUtil::getImagePath("HeaderIcon.svg"));
+    $tpl->setCurrentBlock("content");
+    $tpl->setVariable("ERROR_MESSAGE", $lng->txt('no_document_available'));
+    $tpl->setVariable("MESSAGE_HEADING", $lng->txt('error_sry_error'));
+
+    $tpl->show();
 }
