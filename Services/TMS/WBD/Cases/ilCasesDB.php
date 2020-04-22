a4e840e3dc375579498019ebbe13572e623ec75c
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2020-04-22 09:01:16 +0200

    TMS 4562 include wbd error table at collecting participations to cancel

--- Services/TMS/WBD/Cases/ilCasesDB.php
+++ Services/TMS/WBD/Cases/ilCasesDB.php
@@ -347,17 +347,22 @@ class ilCasesDB implements DB
     public function getParticipationsToCancel() : array
     {
         $q = "SELECT wbd.crs_id, wbd.usr_id, wbd.gutberaten_id, wbd.wbd_booking_id," . PHP_EOL
-            . " wbd.title, wbd.minutes" . PHP_EOL
+            . " wbd.title, wbd.minutes, err.status" . PHP_EOL
             . " FROM xwbd_report_crs_values wbd " . PHP_EOL
             . " JOIN hhd_usrcrs husrcrs ON husrcrs.crs_id = wbd.crs_id" . PHP_EOL
             . "     AND husrcrs.wbd_booking_id = wbd.wbd_booking_id" . PHP_EOL
             . " JOIN hhd_crs hcrs ON hcrs.crs_id = wbd.crs_id" . PHP_EOL
             . "     AND husrcrs.usr_id = wbd.usr_id" . PHP_EOL
-            . " WHERE hcrs.title != wbd.title " . PHP_EOL
-            . "     OR IF(husrcrs.idd_learning_time IS NOT NULL, husrcrs.idd_learning_time, hcrs.idd_learning_time) != wbd.minutes" . PHP_EOL
-            . "     OR IF((hcrs.begin_date IS NOT NULL AND hcrs.begin_date != '0001-01-01'), hcrs.begin_date, husrcrs.booking_date) != wbd.begin_date" . PHP_EOL
-            . "     OR IF((hcrs.end_date IS NOT NULL AND hcrs.end_date != '0001-01-01'), hcrs.end_date, husrcrs.ps_acquired_date) != wbd.end_date"
-            . "     OR husrcrs.booking_status IN ('cancelled', 'cancelled_after_deadline')"
+            . " LEFT JOIN wbd_request_errors err ON err.usr_id = husrcrs.usr_id" . PHP_EOL
+            . "     AND err.crs_id = husrcrs.crs_id" . PHP_EOL
+            . " WHERE (" . PHP_EOL
+            . "     hcrs.title != wbd.title " . PHP_EOL
+            . "         OR IF(husrcrs.idd_learning_time IS NOT NULL, husrcrs.idd_learning_time, hcrs.idd_learning_time) != wbd.minutes" . PHP_EOL
+            . "         OR IF((hcrs.begin_date IS NOT NULL AND hcrs.begin_date != '0001-01-01'), hcrs.begin_date, husrcrs.booking_date) != wbd.begin_date" . PHP_EOL
+            . "         OR IF((hcrs.end_date IS NOT NULL AND hcrs.end_date != '0001-01-01'), hcrs.end_date, husrcrs.ps_acquired_date) != wbd.end_date" . PHP_EOL
+            . "         OR husrcrs.booking_status IN ('cancelled', 'cancelled_after_deadline')" . PHP_EOL
+            . " ) AND" . PHP_EOL
+            . "     (err.status IS NULL OR err.status IN ('resolved'))"
         ;
 
         $res = $this->db->query($q);
