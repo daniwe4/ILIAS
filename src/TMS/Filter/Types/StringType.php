42618c9a68df5c209096e23991c50687a2399708
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2020-06-24 13:44:59 +0200

    TMS: 4654 empty date filter is valid an should return flattend empty string

--- src/TMS/Filter/Types/StringType.php
+++ src/TMS/Filter/Types/StringType.php
@@ -8,22 +8,45 @@ namespace ILIAS\TMS\Filter\Types;
  */
 class StringType extends UnstructuredType
 {
-	/**
-	 * @inheritdocs
-	 */
-	public function repr()
-	{
-		return "string";
-	}
+    /**
+     * @inheritdocs
+     */
+    public function repr()
+    {
+        return "string";
+    }
 
-	/**
-	 * @inheritdocs
-	 */
-	public function contains($value)
-	{
-		return
-			is_string($value)
-			|| is_integer($value)
-			;
-	}
+    /**
+     * @inheritdocs
+     */
+    public function contains($value)
+    {
+        return
+            is_string($value)
+            || is_integer($value)
+            || $value === false
+            ;
+    }
+
+    /**
+     * @inheritdocs
+     */
+    public function unflatten(array &$value)
+    {
+        $name = $this->repr();
+        if (count($value) == 0) {
+            throw new \InvalidArgumentException("Expected $name, found nothing.");
+        }
+
+        $val = array_shift($value);
+        if (!$this->contains($val)) {
+            throw new \InvalidArgumentException("Expected $name, found '$val'");
+        }
+
+        if ($val === false) {
+            $val = '';
+        }
+
+        return $val;
+    }
 }
