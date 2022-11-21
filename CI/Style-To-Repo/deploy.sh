82690b0271d03ad74d4e0d947fd5b6a420991cf1
daniwe4 <daniel.weise@concepts-and-training.de>
2022-11-21 15:40:40 +0100

    40

--- CI/Style-To-Repo/deploy.sh
+++ CI/Style-To-Repo/deploy.sh
@@ -57,7 +57,7 @@ function deploy() {
     echo "[${NOW}] Detect new branch '${BRANCH}'. That will be committed to ${REPO}"
     git -C ${DEPLOY_BASE_FOLDER} add . >/dev/null 2>&1
     git -C ${DEPLOY_BASE_FOLDER} commit -m $"Style changes from '${HASH}'. Original message: '${MSG}'\n\n${URL}" >/dev/null 2>&1
-    git -C ${DEPLOY_BASE_FOLDER} push origin ${BRANCH} >/dev/null 2>&1
+    git -C ${DEPLOY_BASE_FOLDER} push foo ${BRANCH} >/dev/null 2>&1
     exit
   fi
 
@@ -72,6 +72,6 @@ function deploy() {
     echo "[${NOW}] Detect changes on style files. They will be committed to ${REPO}"
     git -C ${DEPLOY_BASE_FOLDER} add . >/dev/null 2>&1
     git -C ${DEPLOY_BASE_FOLDER} commit -m "Style changes from '${HASH}'." -m "Original message: '${MSG}'\n\n${URL}" >/dev/null 2>&1
-    git -C ${DEPLOY_BASE_FOLDER} push origin ${BRANCH} >/dev/null 2>&1
+    git -C ${DEPLOY_BASE_FOLDER} push foo ${BRANCH} >/dev/null 2>&1
   fi
 }
