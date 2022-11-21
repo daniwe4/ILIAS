74444bf696a96f4b127049c4341c22be4b4a6408
daniwe4 <daniel.weise@concepts-and-training.de>
2022-11-21 15:45:08 +0100

    40

--- CI/Style-To-Repo/deploy.sh
+++ CI/Style-To-Repo/deploy.sh
@@ -48,7 +48,7 @@ function deploy() {
 
   cp -r CI/Style-To-Repo/style/* ${DEPLOY_BASE_FOLDER}
 
-  git -C ${DEPLOY_BASE_FOLDER} remote add origin ${REPO_TOKEN}
+  git -C ${DEPLOY_BASE_FOLDER} remote set-url origin ${REPO_TOKEN}
 
   if [ "${NEW_BRANCH}" == "1" ]
   then
