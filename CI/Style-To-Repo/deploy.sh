439b124dd6b5cf97863d934aa836af0272d7ee1a
daniwe4 <daniel.weise@concepts-and-training.de>
2023-06-29 09:17:01 +0200

    uhu

--- CI/Style-To-Repo/deploy.sh
+++ CI/Style-To-Repo/deploy.sh
@@ -63,6 +63,7 @@ function deploy() {
   fi
 
   git -C ${DEPLOY_BASE_FOLDER} update-index --really-refresh >/dev/null 2>&1
+  echo "$(git -C ${DEPLOY_BASE_FOLDER} diff-index HEAD)"
   git -C ${DEPLOY_BASE_FOLDER} diff-index --exit-code HEAD
   CHECK1=$?
   test -z "$(git ls-files --others)"
